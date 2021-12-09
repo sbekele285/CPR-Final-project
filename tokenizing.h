c867b3538cbfbf95d0d63fbd6f4c1c54c0f52eaa
sbekele285 <84488579+sbekele285@users.noreply.github.com>
2021-12-09 08:34:23 -0500

    Add files via upload

--- tokenizing.h
+++ tokenizing.h
@@ -2,7 +2,7 @@
   Student Id  : 125586214
   Email       : sbekele@myseneca.ca
   Course      : CPR101NBB
-  Date        : Dec 9, 2021*/
+  Date        : Dec 5, 2021*/
 
 #ifndef _TOKENIZING_H //checks if TOKENIZING_H has been defined as it always check if the specified identifier is dfined.   
 #define _TOKENIZING_H// Include the header with function prototype.
@@ -10,7 +10,7 @@
 #include <stdio.h>// Includes the input / output related function in our code.
 #include <string.h>// contains macro definitions, constants and declarationsof functions and types used not only for strings handling but also memory handling fuctions.
 
-void tokenizing ();// Function declaration proto type of Tokenising returns no value. 
+void tokenizing();// Function declaration proto type of Tokenising returns no value. 
 
 #endif// this function dtermines if the preprocessing of #,#ifdef,or ifndef has been completed successfully.
 
